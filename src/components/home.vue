69042f34ec22483dec350ece1e50b2c9072d8ed9
panyuhui <1137498085@qq.com>
2020-01-02 15:33:03 +0800

    开始图片分享模块

--- src/components/home.vue
+++ src/components/home.vue
@@ -13,7 +13,7 @@
         </router-link>
       </div>
        <div>
-        <router-link to='/'>
+        <router-link to='/shareimg'>
           <img src="..\assets\images\jiu\menu2.png" alt="" />
           <span>图片分享</span>
         </router-link>
--- src/router/index.js
+++ src/router/index.js
@@ -2,9 +2,14 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 // 引入home 组件
 import Home from '../components/home.vue'
+// 引入图片展示组件
+import ShareImg from '../components/share_img.vue'
 Vue.use(VueRouter)
 
-const routes = [{ path: '/home', component: Home }]
+const routes = [
+  { path: '/home', component: Home },
+  { path: '/shareimg', component: ShareImg }
+]
 
 const router = new VueRouter({
   routes
