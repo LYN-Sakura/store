b717b0d415171c754ffededd32354b006fe1550c
sakura <lyn-sakura@qq.com>
2020-01-02 16:16:52 +0800

    frame组件化

---
+++ src/components/frame/Footerframe.vue
@@ -0,0 +1,56 @@
+<template>
+  <footer>
+    <van-row type="flex">
+      <van-col span="6">
+        <router-link to="/">
+          <i class="iconfont icon-index-fill icon"></i>
+          <div>首页</div>
+        </router-link>
+      </van-col>
+      <van-col span="6">
+        <router-link to="/">
+          <i class="iconfont icon-icon_zhanghao icon"></i>
+          <div>会员</div>
+        </router-link>
+      </van-col>
+      <van-col span="6">
+        <router-link to="/">
+          <van-icon name="cart-o" info="9" />
+          <div>购物车</div>
+        </router-link>
+      </van-col>
+      <van-col span="6">
+        <router-link to="/">
+          <van-icon name="search" />
+          <div>搜索</div>
+        </router-link>
+      </van-col>
+    </van-row>
+  </footer>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style lang="less" scoped>
+footer {
+  width: 100%;
+  height: 40px;
+  padding-top: 10px;
+  background-color: white;
+  a {
+    color: #7d7e80;
+  }
+  div {
+    font-size: 12px;
+  }
+}
+.van-icon {
+  font-size: 18px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -9,10 +9,18 @@ import './assets/css/global.css'
 import './plugins/vant.js'
 // 引入iconfont图标
 import './assets/font/iconfont.css'
+// 引入头部组件
+import HeaderFrame from './components/frame/Headerframe.vue'
+// 引入脚部组件
+import FooterFrame from './components/frame/Footerframe.vue'
 Vue.config.productionTip = false
 Vue.prototype.$http = axios
 // 配置请求根路径
 axios.defaults.baseURL = `http://www.liulongbin.top:3005/`
+
+// 注册全局组件
+Vue.component('HeaderFrame', HeaderFrame)
+Vue.component('FooterFrame', FooterFrame)
 new Vue({
   router,
   store,
