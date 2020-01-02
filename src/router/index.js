2e5fe6067e12d0ee6bdbdfba15a7888dcda839bd
sakura <lyn-sakura@qq.com>
2020-01-02 16:27:20 +0800

    删除frame文件，优化组件化开发

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,17 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
-// 引入主体框架路由
-import Frame from '../components/Frame.vue'
 // 引入主页
 import Home from '../components/home.vue'
 Vue.use(VueRouter)
 
 const routes = [{
   path: '/',
-  redirect: '/frame'
+  redirect: '/home'
 },
 {
-  path: '/frame',
-  component: Frame,
-  redirect: '/home',
-  children: [{
-    path: '/home',
-    component: Home
-  }]
+  path: '/home',
+  component: Home
 }
 ]
 
