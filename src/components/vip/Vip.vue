48fc4e504619177ab34b57059e541210e794bb11
sakura <lyn-sakura@qq.com>
2020-01-04 14:17:32 +0800

    暂存

--- src/components/vip/Vip.vue
+++ src/components/vip/Vip.vue
@@ -11,14 +11,14 @@
 
       <van-grid :column-num="5"><van-grid-item v-for="item in 5" :key="item" icon="alipay" text="支付宝" /></van-grid>
       <van-collapse class="box" v-model="activeNames" :accordion="false">
-        <van-collapse-item title="全部订单" name="1" icon="shopping-cart">内容</van-collapse-item>
-        <van-collapse-item title="分销中心" name="2" icon="phone-circle">内容</van-collapse-item>
-        <van-collapse-item title="我的优惠劵" name="3" icon="label">内容</van-collapse-item>
-        <van-collapse-item title="我的红包" name="4" icon="gift">内容</van-collapse-item>
-        <van-collapse-item title="我的会员卡" name="5" icon="vip-card">内容</van-collapse-item>
-        <van-collapse-item title="我的心愿单" name="6" icon="chat">内容</van-collapse-item>
-        <van-collapse-item title="我的收藏" name="7" icon="column">内容</van-collapse-item>
-        <van-collapse-item title="我的购物村" name="8" icon="cart-circle">内容</van-collapse-item>
+        <van-collapse-item title="全部订单" name="1" icon="shopping-cart">您的肾宝片正在配送...</van-collapse-item>
+        <van-collapse-item title="分销中心" name="2" icon="phone-circle">六味地黄丸旗舰店</van-collapse-item>
+        <van-collapse-item title="我的优惠劵" name="3" icon="label">100元 兰博基里 优惠券</van-collapse-item>
+        <van-collapse-item title="我的红包" name="4" icon="gift">2.22元</van-collapse-item>
+        <van-collapse-item title="我的会员卡" name="5" icon="vip-card">学生卡</van-collapse-item>
+        <van-collapse-item title="我的心愿单" name="6" icon="chat">x9ti-r</van-collapse-item>
+        <van-collapse-item title="我的收藏" name="7" icon="column">知乎官网</van-collapse-item>
+        <van-collapse-item title="我的购物村" name="8" icon="cart-circle">华强北</van-collapse-item>
       </van-collapse>
       <FooterFrame vipLight="#1989fa"></FooterFrame>
     </div>
